090551ac911101c0558b8f49799573d8d57303c4
unknown <david.omeara91@gmail.com>
2020-02-20 11:32:35 +0100

    added css, js, index pages for walking and cycling

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.core.models import Page, Orderable
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, MultiFieldPanel, InlinePanel, PageChooserPanel
+from wagtail.search import index
+from wagtail.images.edit_handlers import ImageChooserPanel
+from modelcluster.fields import ParentalKey
+
+
+# Index Page for Cycling
+class CyclingIndexPage(Page):
+    template =  'blog/cycling/index.html'
+
+    # Banner
+    banner_image = models.ForeignKey(
+        'wagtailimages.image',
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    banner_subtitle = models.CharField(max_length=200)
+
+    # Content
+    content = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        ImageChooserPanel('banner_image'),
+        FieldPanel('banner_subtitle'),
+        FieldPanel('content'),
+    ]
+
+# Index Page for Walking
+class WalkingIndexPage(Page):
+    template =  'blog/walking/index.html'
+
+    # Banner
+    banner_image = models.ForeignKey(
+        'wagtailimages.image',
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    banner_subtitle = models.CharField(max_length=200)
+
+    # Content
+    content = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        ImageChooserPanel('banner_image'),
+        FieldPanel('banner_subtitle'),
+        FieldPanel('content'),
+    ]
+
